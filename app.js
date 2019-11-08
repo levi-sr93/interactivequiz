bb11b6c767e498412dd3a097d6bb28b675b5c35a
Levi Santos R <levi-sr@outlook.com>
2019-11-08 10:43:02 -0300

    Interactive Programming Quiz - Source Code

---
+++ app.js
@@ -0,0 +1,38 @@
+const correctAnswers = ['B', 'B', 'A', 'B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+const span = document.querySelector('span');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    //check Answers
+    userAnswers.forEach((answer, index) => {
+        if(answer === correctAnswers[index]){
+            score += 25;
+        }
+    });
+
+    
+
+
+    //Showing the results on page
+    scrollTo(0,0)
+
+    result.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+
+        if(output === score){
+            clearInterval(timer);
+        } else {
+            output++;
+        }
+    }, 10);
+});
